411ec294ba0666f2eb7ce0acd154473c138b3df3
Railson25 <railsonsantiago123@gmail.com>
2023-07-19 16:29:31 -0300

    <feat>: HeroButton Component

---
+++ src/components/HeroButton/HeroButtons.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import './HeroButton.css'
+
+export const HeroButtons = () => {
+    return(
+        <div className="hero-buttons">
+           <buttons className="btn">Get Started</buttons> 
+           <buttons className="btn">Learn More</buttons> 
+        </div>
+    )
+}
