6e2a488f94b9ed5dbe92de3083ff5348431b51a0
Railson25 <railsonsantiago123@gmail.com>
2023-07-19 16:30:08 -0300

    <feat>: TheBestButton Component

---
+++ src/components/TheBestButton/TheBest.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './TheBest.css'
+
+export const TheBest = () => {
+    return(
+        <div className="the-best-ad">
+            <div className="the-best-ad-div"></div>
+            <span className="the-best-ad-title">The best fitness club in the town.</span>
+        </div>
+
+    )
+}
