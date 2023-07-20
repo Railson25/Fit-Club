ae0db0ad80aa40132df32eb0235d97a04117562c
Railson25 <railsonsantiago123@gmail.com>
2023-07-20 17:00:16 -0300

    <feat>: Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import './Footer.css'
+import { Github, Instagram, LinkedIn, Logo } from "../../assets";
+
+export const Footer = () => {
+    return(
+        <div className="footer-container">
+            <hr />
+
+            <div className="footer">
+                <div className="social-links">
+                    <img src={Github} alt="" />
+                    <img src={Instagram} alt="" />
+                    <img src={LinkedIn} alt="" />
+                </div>
+                <div className="logo-f">
+                    <img src={Logo} alt="" />
+                </div>
+            </div>
+        </div>
+    )
+}
