c1aa1555c41aa45569892e1ff922912a8a9ddff6
Railson25 <railsonsantiago123@gmail.com>
2023-07-20 16:45:22 -0300

    <feat>: Join component

---
+++ src/components/Join/Join.jsx
@@ -0,0 +1,47 @@
+import React, { useRef } from "react";
+
+import './Join.css'
+
+import emailjs from '@emailjs/browser'
+
+export const Join = () => {
+    const form = useRef()
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+    
+        emailjs.sendForm('service_83mhldw', 'template_yoca3tr', form.current, 'KaxUaiXqUEowmkckM')
+          .then((result) => {
+              console.log(result.text);
+          }, (error) => {
+              console.log(error.text);
+          });
+      };
+
+    return (
+        <div className="join" id="join-us">
+            <div className="left-j">
+                <hr />
+                <div>
+                    <span className="stroke-text">READY TO</span>
+                    <span>LEVEL UP</span>
+                </div>
+                <div>
+                    <span>YOUR BODY</span>
+                    <span className="stroke-text">WITH US?</span>
+                </div>
+
+            </div>
+            <div className="right-j">
+                <form ref={form} onSubmit={sendEmail} action="" className="email-container" name="user_email">
+                    <input 
+                        type="email" 
+                        name="user_email" 
+                        placeholder="Enter your Email address"
+                    />
+                    <button className="btn btn-j">Join Now</button>
+                </form>
+            </div>
+        </div>
+    )
+}
