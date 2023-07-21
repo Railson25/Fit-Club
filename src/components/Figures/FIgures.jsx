732b44c38545b8e373c6ec943f1fe9e9b32be5bd
Railson25 <railsonsantiago123@gmail.com>
2023-07-21 15:58:02 -0300

    <style>: Count Number

--- src/components/Figures/FIgures.jsx
+++ src/components/Figures/FIgures.jsx
@@ -1,20 +1,41 @@
 import React from 'react'
-
+import NumberCount from 'number-counter'
 import './Figures.css'
 
 export const Figures = () => {
     return(
         <div className="figures">
             <div className='figures-container'>
-                <span className='figures-number'>+140</span>
+                <span className='figures-number'>
+                    <NumberCount 
+                        end={140} 
+                        start={100}
+                        delay='4'
+                        preFix="+"  
+                    />
+                </span>
                 <span className='figures-text'>expert coaches</span>
             </div>
             <div className='figures-container'>
-                <span className='figures-number'>+978</span>
+                <span className='figures-number'>
+                <NumberCount 
+                        end={978} 
+                        start={940}
+                        delay='4'
+                        preFix="+"  
+                    />
+                </span>
                 <span className='figures-text'>members joined</span>
             </div>
             <div className='figures-container'>
-                <span className='figures-number'>+50</span>
+                <span className='figures-number'>
+                <NumberCount 
+                        end={50} 
+                        start={35}
+                        delay='4'
+                        preFix="+"  
+                    />
+                </span>
                 <span className='figures-text'>fitness programs</span>
             </div>
         </div>
